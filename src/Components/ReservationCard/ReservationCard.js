c040f3069b5a90e1ad3ade75ff4c68f3c80fbc2c
bhohnco <bryanhohn13@gmail.com>
2021-05-26 10:07:46 -0600

    Card build

--- src/Components/ReservationCard/ReservationCard.js
+++ src/Components/ReservationCard/ReservationCard.js
@@ -2,7 +2,15 @@ import React from "react"
 import './ReservationCard.css';
 
 const ReservationCard = (props) => {
-
+  return (
+      <article className="reservation-card" id={props.id} >
+        <h2>{props.name}</h2>
+        <p>{props.date}</p>
+        <p>{props.time}</p>
+        <p>Number of Guests: {props.number}</p>
+        <button className="cancel-res-button">Cancel</button>
+      </article>
+  )
 }
 
 export default ReservationCard;
--- src/Components/Reservations/Reservations.js
+++ src/Components/Reservations/Reservations.js
@@ -3,7 +3,14 @@ import ReservationCard from "../ReservationCard/ReservationCard"
 import "./Reservations.css"
 
 const ReservationCard = ({ reservations }) => {
-  const reservationBookings = reservations.map(reservations => {
+  const reservationBookings = reservations.map(reservation => {
     return <ReservationCard
+        key={reservation.id}
+        id={reservation.id}
+        name={reservation.name}
+        date={reservation.date}
+        time={reservation.time}
+        number={reservation.number}
+        />
   })
 }
