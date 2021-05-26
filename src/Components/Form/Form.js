1f65e6fc2773929a8e7d9a74a10fd358f885bed3
bhohnco <bryanhohn13@gmail.com>
2021-05-26 10:39:49 -0600

    Form constructor start

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Form.css"
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      date: "",
+      time: "",
+      number: "",
+    }
+  }
+
+}
+
+export default Form
