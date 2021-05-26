f7843fea96b0203e7ec745d8fe5cb8d31531e5a8
bhohnco <bryanhohn13@gmail.com>
2021-05-26 10:54:24 -0600

    SubmitReservation add

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -12,6 +12,33 @@ class Form extends Component {
     }
   }
 
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  submitReservation = (event) => {
+    event.preventDefault()
+    const newReservation = {
+      id: Date.now(),
+      name: this.state.name,
+      date: this.state.date,
+      time: this.state.time,
+      number: Number(this.state.number)
+    }
+    this.props.addNewReservation(newReservation)
+    this.setState({name: "", date: "", time: "", number: ""})
+    console.log(this.state)
+  }
+
+  render () {
+    return (
+        <form className= "form-container" onSubmit={this.submitReservation}>
+
+
+        </form>
+    )
+  }
 }
 
 export default Form
